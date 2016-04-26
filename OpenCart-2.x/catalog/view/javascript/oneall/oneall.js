e86caab20f33d1f4225913bcc4303d5a3ddfc23e
cschlesser <cschlesser@oneall.com>
2016-04-26 18:24:35 +0200

    console exist checking

--- OpenCart-2.x/catalog/view/javascript/oneall/oneall.js
+++ OpenCart-2.x/catalog/view/javascript/oneall/oneall.js
@@ -4,18 +4,19 @@
 	var src = scripttags[scripttags.length - 1].src;
 	var parts = unescape(src).split('?domain=');
 	if (parts.length != 2) {
-		console.log('OneAll: missing subdomain name. Library will not be loaded.');
-		return;
+		if (window.console) {
+			console.log('OneAll: missing subdomain name. Library will not be loaded.');
+		}
+	} else {
+		var partsv = parts[1];
+		var partsva = partsv.split('&lang=');
+		var domain = partsva[0];
+		var lang = partsva.length == 2 ? ('?lang=' + partsva[1]) : '';
+		
+		// Load the library:
+	    var oa = document.createElement('script');
+	    oa.type = 'text/javascript'; oa.async = true;
+	    oa.src = '//' + domain + '.api.oneall.com/socialize/library.js' + lang;
+	    scripttags[0].parentNode.insertBefore(oa, scripttags[0]);
 	}
-	var partsv = parts[1];
-	var partsva = partsv.split('&lang=');
-	var domain = partsva[0];
-	var lang = partsva.length == 2 ? ('?lang=' + partsva[1]) : '';
-	// Load the library:
-    var oa = document.createElement('script');
-    oa.type = 'text/javascript'; 
-    oa.async = true;
-    oa.src = '//' + domain + '.api.oneall.com/socialize/library.js' + lang;
-    var s = scripttags[0];
-    s.parentNode.insertBefore(oa, s);
 })();
